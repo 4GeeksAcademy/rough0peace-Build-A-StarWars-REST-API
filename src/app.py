01ba2e4e673d1b27bad98f51667dee77e978b302
Ian Garvey <ianarthurgarvey@gmail.com>
2025-06-13 05:21:36 +0000

    adds classes to models.py, admin.py adds models for User, Person, Planet, Favorites to admin.py, adds routes and fetch responses to app.py

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Person, Planet, Favorites 
 #from models import Person
 
 app = Flask(__name__)
@@ -36,14 +36,55 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    people = Person.query.all() 
+    person_dictionaries = []
+    for person in people: 
+        person_dictionaries.append(
+            person.serialize()
+        )
+    return jsonify(person_dictionaries), 200 
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_one_person():
+    pass
 
-    return jsonify(response_body), 200
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    pass
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_one_planet(): 
+    pass
+
+@app.route('/users', methods=['GET'])
+def get_all_users():
+    pass
+
+@app.route('/users/favorites', methods=['GET'])
+def get_user_favorites():
+    pass
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet():
+    pass
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_favorite_person():
+    pass
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet():
+    pass
+
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_person():
+    pass
+
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,19 +1,79 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    __tablename__ = "User"
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    login_status: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
-
-    def serialize(self):
+    def __repr__(self):
+        return '<User %r>' % self.email
+    
+    def serialize(self): 
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "email": self.email
         }
+
+
+class Person(db.Model):
+    __tablename__ = "Person"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    gender: Mapped[str] = mapped_column(nullable=True)
+    eye_color: Mapped[str] = mapped_column(nullable=True)
+    hair_color: Mapped[str] = mapped_column(nullable=True)
+    favorites = relationship("Favorites", backref="person", lazy=True)
+
+    def serialize(self): 
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "eye_color": self.eye_color,
+            "hair_color": self.hair_color,
+            "favorites": self.favorites
+        }
+
+class Planet(db.Model):
+    __tablename__ = "Planet"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    population: Mapped[str] = mapped_column(nullable=True)
+    terrain: Mapped[str] = mapped_column(nullable=True)
+    climate: Mapped[str] = mapped_column(nullable=True)
+    favorites = relationship("Favorites", backref="planet", lazy=True)
+
+    def serialize(self): 
+        return {
+            "id": self.id,
+            "name": self.name,
+            "population": self.population,
+            "terrain": self.terrain,
+            "climate": self.climate,
+            "favorites": self.favorites
+        }
+
+class Favorites(db.Model):
+    __tablename__ = "Favorites"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("User.id"), nullable=False)
+    person_id: Mapped[int] = mapped_column(
+        ForeignKey("Person.id"))
+    planet_id: Mapped[int] = mapped_column(
+        ForeignKey("Planet.id"))
+  
+    def seralize(self): 
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "person_id": self.person_id,
+            "planet_id": self.planet_id
+        }
+       
